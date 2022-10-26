412ac360c9723d67e2879d63402f68da7fe8ecbc
cagatay <cagatay.gedik@icloud.com>
2022-10-27 02:26:51 +0300

    Implemented UICollectionView(protocols & delegates)

--- Matching Cards/ViewController.swift
+++ Matching Cards/ViewController.swift
@@ -7,9 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
     
     
     let model = CardModel()
@@ -20,6 +21,30 @@ class ViewController: UIViewController {
         
         cardsArray = model.getCards()
         
+        // Set the view controller as the datasource and the delegate of the collection view
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+    }
+    
+    // MARK: - Collection View Delegate Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // Return number of cards
+        return cardsArray.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Get a cell
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: Configure it
+        
+        // Return it
+        return cell
     }
 
 
