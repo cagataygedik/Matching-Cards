cc96e79968fc9cd3287ed23417263b288aafa435
cagatay <cagatay.gedik@icloud.com>
2022-10-26 19:25:18 +0300

    added Card class

---
+++ Matching Cards/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Matching Cards
+//
+//  Created by Celil Çağatay Gedik on 26.10.2022.
+//
+
+import Foundation
+
+class Card {
+    
+    var imageName = ""
+    var isMatched = false
+    var isFlipped = false
+}
+
