0fd92a7c237952a5ab2736a246d00cde8d4103e3
cagatay <cagatay.gedik@icloud.com>
2022-10-26 19:25:54 +0300

    added ViewController

--- Matching Cards/ViewController.swift
+++ Matching Cards/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    let model = CardModel()
+    var cardsArray = [Card]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cardsArray = model.getCards()
+        
     }
 
 
