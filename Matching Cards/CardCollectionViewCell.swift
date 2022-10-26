b9fff30423a16431254c6d5e7b3ad7d262c75d66
cagatay <cagatay.gedik@icloud.com>
2022-10-26 19:26:04 +0300

    implanted the background

---
+++ Matching Cards/CardCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CardCollectionViewCell.swift
+//  Matching Cards
+//
+//  Created by Celil Çağatay Gedik on 26.10.2022.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+}
