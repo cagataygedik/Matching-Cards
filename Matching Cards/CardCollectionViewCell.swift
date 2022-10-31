ac8f83dde7a2f2931d5f40ff6d2f863d6e6a377b
cagatay <cagatay.gedik@icloud.com>
2022-10-31 16:35:29 +0300

    added flip animation

--- Matching Cards/CardCollectionViewCell.swift
+++ Matching Cards/CardCollectionViewCell.swift
@@ -13,4 +13,43 @@ class CardCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var backImageView: UIImageView!
     
+    var card: Card?
+    
+    func configureCell(card: Card) {
+        
+        // Keep track of the card this cell represents
+        self.card = card
+        
+        // Set the front image view to image that represents the card
+        frontImageView.image = UIImage(named: card.imageName)
+        
+        // Reset the state of the cell by checking the flipped status of the card and then showing the front or the back imageview accordingly
+        if card.isFlipped == true {
+            // Show the front image view
+            flipUp(speed: 0)
+        }
+        else {
+            // Show the back image view
+            flipDown(speed: 0)
+        }
+    }
+    
+    func flipUp(speed:TimeInterval = 0.3) {
+        
+        // Flip up animation
+        UIView.transition(from: backImageView, to: frontImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+        
+        // Set the status of the card
+        card?.isFlipped = true
+    }
+    
+    func flipDown(speed:TimeInterval = 0.3) {
+        
+        // Flip down animation
+        UIView.transition(from: frontImageView, to: backImageView, duration: speed, options: [.showHideTransitionViews, .transitionFlipFromLeft], completion: nil)
+        
+        card?.isFlipped = false
+
+    }
+    
 }
--- Matching Cards/ViewController.swift
+++ Matching Cards/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     let model = CardModel()
     var cardsArray = [Card]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -39,14 +39,36 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // Get a cell
-       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
         
-        // TODO: Configure it
+        // Get the card from card array
+        let card = cardsArray[indexPath.row]
+        
+        // Configure it
+        cell.configureCell(card: card)
         
         // Return it
         return cell
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Get a reference to the cell that was tapped
+        let cell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell
+        
+        
+        //Check the status of the card to determine how to flip it
+        if cell?.card?.isFlipped == false {
+            // Flip the card up
+            cell?.flipUp()
+        }
+        else {
+            // Flip the card down
+            cell?.flipDown()
+        }
+        
+    }
+    
+    
 }
 
